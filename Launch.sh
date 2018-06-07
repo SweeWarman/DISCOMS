2e0425a62caff5fed9dce4546764f05084d9937e
swee <sweewarman@gmail.com>
2018-06-06 23:00:25 -0400

    Fixed launch script

--- Launch.sh
+++ Launch.sh
@@ -2,16 +2,16 @@
 
 if [ "$1" == '1' ];then
     echo "Launching  vehicle 1"
-    python SetupAgent.py "$2" -100 0 0 3 0 0 True
+    python SetupAgent.py "$2" -100 0 3 True
     
 elif [ "$1" == '2' ];then
     echo "Launching  vehicle 2"
-    python SetupAgent.py "$2" 0 -100 0 0 3 0 False
+    python SetupAgent.py "$2" 0 -100 3 False
 
 elif [ "$1" == '3' ];then
     echo "Launching  vehicle 3"
-    python SetupAgent.py "$2" -50 -50 0 1.5 1.5 0 False
+    python SetupAgent.py "$2" -50 -50 3 0 False
     
 else
-    echo "options are 1,2"
+    echo "options are 1,2,3"
 fi
